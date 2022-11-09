a2d5c04c6a26510877246937393239b520386613
unknown <nathan.manaranche@gmail.com>
2022-11-09 11:56:28 +0100

    3 - return result addition test

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -1,17 +1,23 @@
 const test = require("tape");
 const Calcul = require("../index");
 
-// test("Return nothing", (t) => {
-//   const expr = "";
-//   const calcul = new Calcul();
-//   t.equal(calcul.rpn(expr), undefined);
-//   t.end();
-// });
-
+test("Return nothing", (t) => {
+  const expr = "";
+  const calcul = new Calcul();
+  t.equal(calcul.rpn(expr), undefined);
+  t.end();
+});
 
 test("Return int", (t) => {
-    const expr = "3";
-    const calcul = new Calcul();
-    t.equal(calcul.rpn(expr), 3);
-    t.end();
-  });
+  const expr = "3";
+  const calcul = new Calcul();
+  t.equal(calcul.rpn(expr), 3);
+  t.end();
+});
+
+test("Return result addition", (t) => {
+  const expr = "2 3 +";
+  const calcul = new Calcul();
+  t.equal(calcul.rpn(expr), 5);
+  t.end();
+});
--- index.js
+++ index.js
@@ -3,7 +3,20 @@ class Calcul {
   
     rpn(expr) {
 
-        return parseInt(expr);
+        const e = expr.split(' ');
+
+        if (!e.length) return undefined;
+        if (e.length === 1 && e[0]) return parseInt(e[0]);
+
+        if (e.length < 3) return undefined;
+
+        const e1 = parseInt(e[0]);
+        const e2 = parseInt(e[1]);
+        const op = e[2];
+
+        if (op === "+") return parseInt(e1 + e2);
+
+
     }
   }
   
